6c3bb9c946ae9d32343b164b8675aada1bdb414b
bigfile57 <bigfile57@naver.com>
2020-04-08 16:54:37 +0900

    modify2

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -10,6 +10,8 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   delayValue -= 150;
+  if(delayValue <0)
+    delayValue = 2000;
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
     delay(delayValue);                       // wait for a second
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
